a89516b77396a9b37d0f8f478e59dcbbb4ec21eb
Ahmad-billz <ahmadbilalsohail@hotmail.com>
2021-04-03 00:31:58 +0200

    add routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
+import {createApp } from 'vue'
 import App from './App.vue'
+  
+import { createWebHistory, createRouter } from "vue-router";
+import ContactsList from "./components/ContactsList"
+import AddContact from "./components/AddContact"
 
-createApp(App).mount('#app')
+const routes = [
+    {
+      path: "/",
+      name: "Contacts",
+      component: ContactsList,
+    },
+    {
+        path: "/add",
+        name: "add",
+        component: AddContact,
+      }
+  ];
+ const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+
+createApp(App).use(router).mount('#app')
